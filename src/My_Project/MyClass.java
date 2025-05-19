075412cb7fcceeb3a53bacd7618393b879970753
InvarGervi123 <InvarGervi1234@gmail.com>
2025-05-19 12:11:46 +0300

    my_frist push	x

---
+++ src/My_Project/MyClass.java
@@ -0,0 +1,608 @@
+package My_Project;
+import java.util.Random;
+import java.util.Scanner;
+public class MyClass 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+//		int day = in.nextInt();
+//		switch(day)
+//		{
+//		case 1: System.out.print("sunday"); break;
+//		case 2: System.out.print("monday"); break;
+//		case 3: System.out.print("tuesday"); break;
+//		case 4: System.out.print("wednesday"); break;
+//		case 5: System.out.print("thursday"); break;
+//		case 6: System.out.print("friday"); break;
+//		case 7: System.out.print("saturday"); break;
+//		default: System.out.print("Invalide day");
+//		}
+//		while (true) 
+//		{
+//		int num1 = in.nextInt();
+//		char ch = in.next().charAt(0); // +-*/%
+//		int num2 = in.nextInt(); // /0 %0 not working
+//		switch(ch)
+//		{
+//		case '+': System.out.print(num1+num2); break;
+//		case '-': System.out.print(num1-num2); break;
+//		case '*': System.out.print(num1*num2); break;
+//		case '/': if (num2 == 0) { System.out.print("do not write on num2 == 0"); } else System.out.print(num1/num2); break;
+//		case '%': if (num2 == 0) { System.out.print("do not write on num2 == 0"); } else System.out.print(num1%num2); break;
+//		default: System.out.print(false); break;
+//		}
+//		}
+//		Scanner in = new Scanner(System.in);
+//		int count = 0;
+//		int num = in.nextInt();
+//		while (count != 0)
+//		{
+//			if (num >= 100 && 1000 > num)
+//			{
+//				count++;
+//			}
+//		}
+//		Scanner in = new Scanner(System.in);
+//		int count=0;
+//		int num1 = in.nextInt();
+//		int num2 = in.nextInt();
+//		while (num1 !=0 && num2 !=0)
+//		{
+//			if ((num1%2==0 && num2%2==0) && (num1%2 !=0 && num2%2 !=0))
+//			{
+//				System.out.print(true);
+//			}
+//
+//			num1 = in.nextInt();
+//			num2 = in.nextInt();
+//		}
+//		Scanner in = new Scanner(System.in);
+//		int num1 = in.nextInt();
+//		int num2 = in.nextInt();
+//		int num3 = in.nextInt();
+//		while (!((num1 == num2) && (num2 == num3)) || !((num1==num3) && (num2 == num3)))
+//		{
+//			num1 = in.nextInt();
+//			num2 = in.nextInt();
+//			num3 = in.nextInt();
+//		}
+//		switch
+//		{
+//		
+//		}
+		///Scanner scanner = new Scanner(System.in);
+		///String name = scanner.nextLine();
+		///int a =5;
+		///if (a==5 && name.equals("a"))
+		///{
+		///	print(true);
+		///}
+		///else
+		///{
+		///	print(false);
+		///}
+		
+		///scanner.close();
+		//print(Soild.get_HP());
+//		Soild soiler1 = new Soild();
+//		Soild soiler2 = new Soild();
+//		Soild soiler3 = new Soild();
+//		Soild[] soilers = {soiler1,soiler2,soiler3};
+//		for (int i = 0;i<soilers.length;i++)
+//		{
+//			System.out.println(soilers[i].get_HP());
+//		}
+//		System.out.print(check(4));
+		//##########################################################
+//		int num = Integer.MIN_VALUE;
+//		int max = num;
+//		int count=1;
+//		while (45>=count)
+//		{
+//			num = (int)(Math.random()*101);
+//			if (num > max)
+//			{
+//				max = num;
+//			}
+//			count++;
+//		}
+//		System.out.print(max);
+//		int num = Integer.MIN_VALUE;
+//		int max = num;
+//		int min = Integer.MAX_VALUE;
+//		int count=1;
+//		while (100 > count)
+//		{
+//			num = (int)(Math.random()*1001);
+//			if (num>max)
+//			{
+//				max = num;
+//			}
+//			if (num<min)
+//			{
+//				min = num;
+//			}
+//			count++;
+//		}
+//		System.out.println(max);
+//		System.out.println(min);
+//		int num = Integer.MIN_VALUE;
+//		int max = num;
+//		int max2 =num;
+//		int count=1;
+//		while (45>=count)
+//		{
+//			num = (int)(Math.random()*101);
+//			if (num > max)
+//			{
+//				max = num;
+//			}
+//			num = (int)(Math.random()*101);
+//			if (num > max2)
+//			{
+//				max2 = num;
+//			}
+//			count++;
+//		}
+//		System.out.println(max);
+//		System.out.println(max2);
+//		int num = Integer.MIN_VALUE;
+//		int max = num;
+//		int count=1;
+//		while (45>=count)
+//		{
+//			num = (int)(Math.random()*101);
+//			if (num > max)
+//			{
+//				max = num;
+//			}
+//			count++;
+//		}
+//		System.out.print(max);
+//		int num = Integer.MIN_VALUE;
+//		int limit = 45;
+//		int max = Integer.MIN_VALUE;
+//		int i = 0;
+//		while(i <= limit)
+//		{
+//			num = in.nextInt();
+//			if(max<num)
+//			{
+//				max = num;
+//			}
+//			i++;
+//		}
+//		System.out.print(max);
+//		int num = 0;
+//		int max = Integer.MIN_VALUE;
+//		int min = Integer.MAX_VALUE;
+//		for (int i=0;i<=3;i++)
+//		{
+//			num = in.nextInt();
+//			while (10>num || num>99)
+//			{
+//				num = in.nextInt();
+//				if (num>max)
+//				{
+//					max = num;
+//				}
+//				else if (num<min)
+//				{
+//					min = num;
+//				}
+//			}
+//		}
+//		System.out.println("max"+max);
+//		System.out.println("min"+min);
+//		for (int i =0;i<4;i++)
+//		{
+//			for(int j = 0;j<i;j++)
+//			{
+//				System.out.print(" ");
+//			}
+//			for(int j = 0;j<4-i;j++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+//		System.out.print(numOfDigits(4546));
+//		System.out.print(bigger(3,2));
+//		System.out.print(numels(945232));
+//		System.out.println(checkbigger(655,5125));
+//		buildstar(4);
+//		System.out.print(buildwords(5,'$'));
+//		builderwords(4,'$');
+//		swap(2,3);
+		//System.out.print(Exo2(2,"aas"));
+//		String[] w1 = {"qwew"};
+//		String[] w2 = {"qwew"};
+//		System.out.print(Exo3(w1,w2));
+//		System.out.print(Exo4("fghfhjff"));
+		//System.out.print(Exo5("12321"));
+		//arraylist();
+		//fuc2();
+		//System.out.print(numbalance(5));
+//		int[] num = {4,5,2,3,3,5,8,2};
+//		backthearray(num);
+//		int[] num = {4,6,7,2};
+//		int[] num2 = {4,6,7,8};
+//		System.out.print(checkarray(num,num2));
+//		int[] num = {1,2,3,4,5};
+//		System.out.print(checkarraytobigger(num));
+//		int[] num = {1,2,3,4,9};
+//		System.out.print(moudelarray(num));
+//		char[] arr = {'G','o','o','d'};
+//		System.out.print(chartostring(arr));
+//		int[] num = {1,2,3,4,5};
+//		System.out.print(sumnum(num));
+		int[] arr = {1,2,3,4,5,6};
+		int num = 3;
+		System.out.print(howmanyarrays(arr,num));
+	}
+	public static int howmanyarrays(int[] arr,int num)
+	{
+		int count =0;
+		for(int i = 0;i<arr.length;i++)
+		{
+			if (arr[i] % num == 0)
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	public static double sumnum(int[] num)
+	{
+		int sum=0;
+		for (int i =0;i<num.length;i++)
+		{
+			sum = sum + num[i];
+		}
+		sum = sum / num.length;
+		return sum;
+	}
+	public static String chartostring(char[] words)
+	{
+		String sentence = "";
+		for (int i =0;i<words.length;i++)
+		{
+			sentence = sentence + words[i];
+		}
+		return sentence;
+	}
+	public static boolean moudelarray(int[] num)
+	{
+		for (int i =0;i<num.length;i=i+2)
+		{
+			if (num[i] % 2 ==0)
+			{
+				return false;
+			}
+		}
+		for (int i=1;i<num.length;i=i+2)
+		{
+			if (num[i] % 2 != 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	public static boolean checkarraytobigger(int[] num)
+	{
+		for (int i =0;i<num.length-1;i++)
+		{
+			if (num[i+1] < num[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	public static boolean checkarray(int[] num,int[] num2)
+	{
+		if (num.length != num2.length)
+		{
+			return false;
+		}
+		for (int i = 0;i < num.length;i++)
+		{
+			if (num[i] != num2[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	public static int[] backthearray(int[] num)
+	{
+		int[] nummin = num;
+		int min = Integer.MAX_VALUE;
+		//int countmin =0;
+		for (int i =0;i<nummin.length;i++)
+		{
+			if (min > nummin[i])
+			{
+				min = nummin[i];
+				//countmin++;
+			}
+		}
+		int[] newmin = new int[nummin.length];
+		for (int i=0;i<nummin.length;i++)
+		{
+			if (nummin[i] != min)
+			{
+				newmin[i] = nummin[i];
+				System.out.println(newmin[i]);
+			}
+		}
+		return newmin;
+	}
+	public static int[] numbalance(int num)
+	{
+		int[] array = new int[num];
+		for (int i =0 ; i<array.length;i++)
+		{
+			array[i] = (int)(Math.random() * 51) + 50;
+			System.out.println(array[i]);
+		}
+		return array;
+	}
+	public static void fuc2()
+	{
+		Scanner in =new Scanner(System.in);
+		char[] crr = new char[10];
+		for (int i =0;i<crr.length;i++)
+		{
+			crr[i] = in.next().charAt(0);
+		}
+		for (int i=0;i<crr.length;i++)
+		{
+			if (crr[i]>='A' && crr[i]<='Z')
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+	public static void arraylist()
+	{
+		Scanner in = new Scanner(System.in);
+		int[] num = new int[10];
+		for (int i =0;i<num.length;i++)
+		{
+			num[i] = in.nextInt();
+		}
+		for (int i =0;i<num.length;i++)
+		{
+			if (num[i] % 2 ==0)
+			{
+				System.out.println(num[i]);
+			}
+		}
+	}
+	public static boolean Exo5(String words)
+	{
+		char steak;
+		int right = 0;
+		int left = 0;
+		for(int i=0;i<words.length();i++)
+		{
+			steak = words.charAt(i);
+			for (int j=0;j<words.length();j++)
+			{
+				if (steak != words.charAt(j))
+				{
+					right++;
+				}
+				else 
+				{
+					left++;
+				}
+			}
+		}
+		if (left>=right)
+		{			
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public static String Exo4(String words)
+	{
+		String steak = "";
+		for (int i = 0;i<words.length()-1;i++)
+		{
+			if (words.charAt(i) ==words.charAt(i+1))
+			{
+				steak = Character.toString(words.charAt(i));
+			}
+		}
+		return steak;
+	}
+	public static String Exo3(String[] words1,String[] words2)
+	{
+		//int max = 0;
+		int max = Math.max(words1.length , words2.length);
+//		if (words1.length() > words2.length())
+//		{
+//			max = words1.length();
+//		}
+//		else
+//		{
+//			max = words2.length();
+//		}
+		for (int i = 0;i<max;i++)
+		{
+			if(words1[i].equals(words2[i]))
+			{
+				return "HAVE";
+			}
+		}
+		return "NOT HAVE";
+	}
+	public static boolean Exo2(int num,String words)
+	{
+		if(num == words.length())
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+//	public static void swap(int x,int y)
+//	{
+//		int old_x = x;
+//		int old_y = y;
+//		x = old_y;
+//		y = old_x;
+//		System.out.print(x+""+y);
+//		//int[] newswap = {x,y};
+//		//return newswap;
+//	}
+//	public static void builderwords(int num,char word)
+//	{
+//		for (int i = 0;i<num;i++)
+//		{
+//			for (int j=num-i-1;j<num;j++)
+//			{
+//				System.out.print(word);
+//			}
+//			System.out.println();
+//		}
+//	}
+//	public static String buildwords(int num,char word)
+//	{
+//		String words ="";
+//		for ( int i=0; i<num;i++)
+//		{
+//			words = words + word;
+//		}
+//		return words;
+//	}
+
+//    public static void buildstar(int num)
+//	{
+//		for (int i = 0; i<num;i++)
+//		{
+//			for (int j =num-i-1;j<num;j++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//	}
+//		
+//	public static int checkbigger(int num,int num2)
+//	{
+//		Random rand = new Random();
+//		int index = 1;
+//		int index2= 1;
+//		int Onum = num;
+//		int Onum2 = num2;
+//		while(num>=10)
+//		{
+//			num = num/10;
+//			index++;
+//		}
+//		while(num2>=10)
+//		{
+//			num2 = num2/10;
+//			index2++;
+//		}
+//		if (index > index2)
+//		{
+//			return Onum;
+//		}
+//		else if (index < index2)
+//		{
+//			return Onum2;
+//		}
+//		if (num % 10 > num2 % 10)
+//		{
+//			return Onum;
+//		}
+//		return Onum2;
+//	}
+//	public static int numels(int num)
+//	{
+//		int index =1;
+//		while(num>=10)
+//		{
+//			num = num/10;
+//			index++;
+//		}
+//		return index;
+//	}
+//	public static int bigger(int num,int num2)
+//	{
+//		int max = num;
+//		if(num2>num)
+//		{
+//			max =num2;
+//		}
+//		return max;
+//	}
+//	public static int numOfDigits(int num)
+//	{
+//		return num%10 + num/100 + num%100/10;
+//	}
+//	public static boolean check(int num)
+//	{
+//		for (int i =2;i<num;i++)
+//		{
+//			
+//			if(num < 1)
+//			{
+//				return false;
+//			}
+//			if(num % i == 0)
+//			{
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//	public static double averageEven(int[] num)
+//	{
+//		int sum = 0;
+//		int index =0;
+//		for (int i =0;i<num.length;i++)
+//		{
+//			if (num[i]%2==0)
+//			{
+//				sum = sum + num[i];				
+//				index++;
+//			}
+//		}
+//		if(index==0)
+//		{
+//			return -1;
+//		}
+//		else
+//		{
+//			return (sum/index);
+//		}
+//	}
+	
+	public static void print(Object words)
+	{
+		System.out.print(String.valueOf(words));
+	}
+
+}
+class Soild
+{
+	int HP = (int)(Math.random() * 101);
+	public int get_HP()
+	{	
+		return HP;
+	}
+}
